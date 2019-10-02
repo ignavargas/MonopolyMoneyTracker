c46fd5d6b72af48d1228ab0b719ae6017c09eadf
ignavargas <ignavaoc17@gmail.com>
2019-10-01 23:55:18 -0600

    Main Activity to Money Tracker link Main Activity logic implementation

--- app/src/main/java/com/example/monopolymoneytracker/MainActivity.java
+++ app/src/main/java/com/example/monopolymoneytracker/MainActivity.java
@@ -1,14 +1,42 @@
 package com.example.monopolymoneytracker;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     private String username;
+    private Button play;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        play = (Button) findViewById(R.id.playButton);
+
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setUsernameValue(play.getText().toString());
+                startGame();
+            }
+        });
+    }
+
+    public void setUsernameValue(String input){
+
+        this.username = input;
+    }
+
+    public void startGame(){
+
+        Intent intent = new Intent(this, MoneyTracker.class);
+        intent.putExtra("username", this.username);
+        startActivity(intent);
     }
 }
