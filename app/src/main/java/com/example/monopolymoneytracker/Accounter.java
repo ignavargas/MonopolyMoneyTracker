e47f9687ac4bb6d161ee748591e808cd46b52c44
ignavargas <ignavaoc17@gmail.com>
2019-10-02 15:54:08 -0600

    Logic Implementation for Accounter \n Logic Implementation for DialogMaker \n dialog layout for input created.

--- app/src/main/java/com/example/monopolymoneytracker/Accounter.java
+++ app/src/main/java/com/example/monopolymoneytracker/Accounter.java
@@ -1,4 +1,30 @@
 package com.example.monopolymoneytracker;
 
 public class Accounter {
+
+    private static final float percent = (float) 0.10;
+
+    public Accounter(){
+    }
+
+    public int updatePayment(int total, int payValue){
+
+
+        total -= payValue;
+
+        return total;
+    }
+
+    public int updateIncome(int total, int payday){
+
+        total +=payday;
+
+        return total;
+    }
+
+    public int getTenPercent(int total){
+
+        int amount = (int) (total * percent);
+        return amount;
+    }
 }
--- app/src/main/java/com/example/monopolymoneytracker/DialogMaker.java
+++ app/src/main/java/com/example/monopolymoneytracker/DialogMaker.java
@@ -1,4 +1,71 @@
 package com.example.monopolymoneytracker;
 
-public class DialogMaker {
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.view.View;
+import android.widget.EditText;
+
+public class DialogMaker extends Activity {
+
+    AlertDialog.Builder dialogBuilder;
+    AlertDialog dialog;
+    private EditText input;
+    private int receivedValue;
+
+    public DialogMaker(){
+        receivedValue = 0;
+    }
+
+    public void showAmount(String title, String message){
+
+        dialogBuilder.setTitle(title);
+        dialogBuilder.setMessage(message);
+
+        dialogBuilder.setNeutralButton("PERFECTO", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                dialog.dismiss();
+            }
+        });
+
+        dialog = dialogBuilder.create();
+        dialog.show();
+    }
+
+    public void registerPayment(){
+
+        Context con = this;
+        dialogBuilder = new AlertDialog.Builder(con);
+        View view = getLayoutInflater().inflate(R.layout.activity_input_dialog, null);
+        dialogBuilder.setView(view);
+        input = (EditText) view.findViewById(R.id.inputValue);
+        dialogBuilder.setPositiveButton("ACEPTAR", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                setReceivedValue(Integer.getInteger(input.getText().toString()));
+                dialog.dismiss();
+            }
+        });
+
+        dialogBuilder.setNegativeButton("CANCELAR", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                dialog.dismiss();
+            }
+        });
+
+        dialog = dialogBuilder.create();
+    }
+
+    public void setReceivedValue(int receivedValue) {
+        this.receivedValue = receivedValue;
+    }
+
+    public int getReceivedValue() {
+        return receivedValue;
+    }
 }
