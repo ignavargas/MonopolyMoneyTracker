89a88f84f6077b77da69e08ff3a65e1d54ca44fb
ignavargas <ignavaoc17@gmail.com>
2019-10-02 23:53:39 -0600

    DialogMaker class elimination due to error on bytecode. And previous awkward behavior. Alert Dialog implementation directly on button action.

--- app/src/main/java/com/example/monopolymoneytracker/MoneyTracker.java
+++ app/src/main/java/com/example/monopolymoneytracker/MoneyTracker.java
@@ -1,7 +1,9 @@
 package com.example.monopolymoneytracker;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -15,7 +17,8 @@ public class MoneyTracker extends AppCompatActivity {
     private String totalMoney;
     private static String percent = "10%";
     Button pay, deal, tax, payday; // there's a fifth button but it isn't in production yet
-    TextView title;
+    TextView title, amount;
+    Accounter accounter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,11 +30,62 @@ public class MoneyTracker extends AppCompatActivity {
         title = (TextView) findViewById(R.id.userName);
         title.setText(this.userName);
 
+        amount = (TextView) findViewById(R.id.totalMoney);
+        this.total = Integer.parseInt(amount.getText().toString());
+
         deal = (Button) findViewById(R.id.dealButon);
         deal.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 setDeal();
+            }
+        });
+
+        tax = (Button) findViewById(R.id.percentButton);
+        tax.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(MoneyTracker.this);
+                dialogBuilder.setTitle("El 10% de su dinero es:");
+               final String percentValue = getPercent(total);
+                dialogBuilder.setMessage(percentValue);
+                dialogBuilder.setNeutralButton("ACEPTAR", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        total -= Integer.parseInt(percentValue);
+                        amount.setText(Integer.toString(total));
+                        dialog.dismiss();
+                    }
+                });
+
+                AlertDialog dialog = dialogBuilder.create();
+                dialog.show();
+
+            }
+        });
+
+        payday = (Button) findViewById(R.id.goButton);
+        payday.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(MoneyTracker.this);
+                dialogBuilder.setTitle("Salario | GO!");
+                dialogBuilder.setMessage(" +200 ");
+                dialogBuilder.setNeutralButton("ACEPTAR", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        total += 200;
+                        amount.setText(Integer.toString(total));
+                        dialog.dismiss();
+                    }
+                });
+
+                AlertDialog dialog = dialogBuilder.create();
+                dialog.show();
             }
         });
     }
@@ -57,9 +111,11 @@ public class MoneyTracker extends AppCompatActivity {
 
     public String getPercent(int total){
 
-        String result = "";
+        accounter = new Accounter();
 
-        return result;
+        int result = accounter.getTenPercent(total);
+
+        return Integer.toString(result);
     }
 
     public void setPayday(int income) {
