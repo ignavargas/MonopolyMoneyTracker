93fecd39efcab013f318c8ac6b9c4e10944081f3
ignavargas <ignavaoc17@gmail.com>
2019-10-04 22:17:25 -0600

    Changed name in id edit text dialog.- Payment, Incomes and percentage, implementation and fixes - Other's button taked as slternate income money.

--- app/src/main/java/com/example/monopolymoneytracker/MoneyTracker.java
+++ app/src/main/java/com/example/monopolymoneytracker/MoneyTracker.java
@@ -8,15 +8,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MoneyTracker extends AppCompatActivity {
 
     private String userName;
     private int total = 0;
-    private String totalMoney;
     private static String percent = "10%";
-    Button pay, deal, tax, payday; // there's a fifth button but it isn't in production yet
+    Button pay, deal, tax, payday, win;
+    //Button acceptPay, cancelPay;
+    EditText paymentDialog;
     TextView title, amount;
     Accounter accounter;
 
@@ -27,6 +29,7 @@ public class MoneyTracker extends AppCompatActivity {
 
         Intent intent = getIntent();
         setUserName(intent.getStringExtra("username"));
+
         title = (TextView) findViewById(R.id.userName);
         title.setText(this.userName);
 
@@ -48,14 +51,13 @@ public class MoneyTracker extends AppCompatActivity {
 
                 AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(MoneyTracker.this);
                 dialogBuilder.setTitle("El 10% de su dinero es:");
-               final String percentValue = getPercent(total);
+               final String percentValue = getPercent();
                 dialogBuilder.setMessage(percentValue);
                 dialogBuilder.setNeutralButton("ACEPTAR", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
 
-                        total -= Integer.parseInt(percentValue);
-                        amount.setText(Integer.toString(total));
+                        pay(Integer.parseInt(percentValue));
                         dialog.dismiss();
                     }
                 });
@@ -78,8 +80,71 @@ public class MoneyTracker extends AppCompatActivity {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
 
-                        total += 200;
-                        amount.setText(Integer.toString(total));
+                        setPayday(200);
+                        dialog.dismiss();
+                    }
+                });
+
+                AlertDialog dialog = dialogBuilder.create();
+                dialog.show();
+            }
+        });
+
+        pay = (Button) findViewById(R.id.payButton);
+        pay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(MoneyTracker.this);
+                View view = getLayoutInflater().inflate(R.layout.activity_input_dialog, null);
+                paymentDialog = (EditText) view.findViewById(R.id.inputValueDialog);
+
+                dialogBuilder.setView(view);
+                dialogBuilder.setPositiveButton("ACEPTAR", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        String pago  = paymentDialog.getText().toString();
+                        pay(Integer.parseInt(pago));
+                        dialog.dismiss();
+                    }
+                });
+
+                dialogBuilder.setNegativeButton("CANCELAR", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+
+                AlertDialog dialog = dialogBuilder.create();
+                dialog.show();
+
+            }
+        });
+
+        win = (Button) findViewById(R.id.otherButton);
+        win.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(MoneyTracker.this);
+                View view = getLayoutInflater().inflate(R.layout.activity_input_dialog, null);
+                paymentDialog = (EditText) view.findViewById(R.id.inputValueDialog);
+
+                dialogBuilder.setView(view);
+                dialogBuilder.setPositiveButton("ACEPTAR", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        String income  = paymentDialog.getText().toString();
+                        setPayday(Integer.parseInt(income));
+                        dialog.dismiss();
+                    }
+                });
+
+                dialogBuilder.setNegativeButton("CANCELAR", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
                         dialog.dismiss();
                     }
                 });
@@ -102,23 +167,27 @@ public class MoneyTracker extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public String pay(int value, int total){
+    public void pay(int value){
 
-        String result = "";
+        accounter = new Accounter();
+        this.total = accounter.updatePayment(this.total, value);
+        amount.setText(Integer.toString(this.total));
 
-        return result;
     }
 
-    public String getPercent(int total){
+    public String getPercent(){
 
         accounter = new Accounter();
 
-        int result = accounter.getTenPercent(total);
+        int result = accounter.getTenPercent(this.total);
 
         return Integer.toString(result);
     }
 
     public void setPayday(int income) {
 
+        accounter = new Accounter();
+        this.total = accounter.updateIncome(total, income);
+         amount.setText(Integer.toString(this.total));
     }
 }
